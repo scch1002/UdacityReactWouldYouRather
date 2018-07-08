5934819b20605c9c33f0d9852707c697ac824c7c
Christopher Schwan <ChrisSchwan8009@hotmail.com>
2018-07-08 16:25:00 -0500

    Create Navigation panel and routing

--- wouldyourather/src/App.js
+++ wouldyourather/src/App.js
@@ -1,18 +1,47 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { Route } from 'react-router-dom';
+import {
+  Collapse,
+  Navbar,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink } from 'reactstrap';
+import { QuestionDashboard } from './components/questionDashboard';
 import './App.css';
+import { NewQuestion } from './components/newQuestion';
+import { LeaderBoard } from './components/leaderBoard.js';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+         <Navbar color="light" light expand="md">
+          <NavbarBrand href="/">reactstrap</NavbarBrand>
+            <Nav className="ml-auto" navbar>
+              <NavItem>
+                <NavLink href="/">Home</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink href="/add">New Question</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink href="/leaderboard">Leader Board</NavLink>
+              </NavItem>
+            </Nav>
+          </Navbar>
+          <Route exact path='/' render={() => (
+              <QuestionDashboard />
+          )}>
+          </Route>
+          <Route path='/add' render={() => (
+              <NewQuestion />
+          )}>
+          </Route>
+          <Route path='/leaderboard' render={() => (
+              <LeaderBoard />
+          )}>
+          </Route>
       </div>
     );
   }
---
+++ wouldyourather/src/components/questionDashboard.js
@@ -0,0 +1,3 @@
+
+
+export const QuestionDashboard = (props) => "pollDashboard";

5637c85e3b1a3a03ffbdefffae29f2fb26b3e21a
Schwan <50006@icf.com>
2018-07-26 13:43:15 -0500

    Corrected user reducer to update question array

--- src/reducers/users.js
+++ src/reducers/users.js
@@ -1,4 +1,5 @@
-import { RETRIEVE_USER } from '../actions/users';
+import { RETRIEVE_USER, ANSWER_QUESTION } from '../actions/users';
+import { ADD_NEW_QUESTION } from '../actions/questions';
 
 export default function users(state = [], action) {
     switch(action.type) {
@@ -6,6 +7,30 @@ export default function users(state = [], action) {
             return [
                 ...action.availableUsers
             ]
+        case ANSWER_QUESTION:
+            return state.map(m => m.id === action.userId ? 
+                {
+                    ...m,
+                    answers: {
+                        ...m.answers,
+                        [action.questionId]: action.anwser
+                    }
+                }
+                :
+                m
+            );
+        case ADD_NEW_QUESTION:
+            return state.map(m => m.id === action.newQuestion.author ? 
+                {
+                    ...m,
+                    questions: [
+                        ...m.questions,
+                        action.newQuestion.id
+                    ]
+                }
+                :
+                m
+            );
         default:
             return state;
     }

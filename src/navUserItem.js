3787eb36c2185ae1a11aaab3324906b6103502b9
Schwan <50006@icf.com>
2018-07-27 20:52:39 -0500

    Changed to use bootstrap

--- src/navUserItem.js
+++ src/navUserItem.js
@@ -4,15 +4,14 @@ import { connect } from 'react-redux';
 import {
     NavItem,
     NavLink } from 'reactstrap';
-import './navUserItem.css';
 import { logoutUser } from './actions/users';
 
 export const NavUserItem = connect()((props) => (
     <Fragment>
-        <div className='navbar-text navUserItem-Spacing'>
+        <div className='navbar-text mr-3'>
             {`Hello, ${props.userInfo.name}`}
         </div>
-        <img className='navbar-text navUserItem-Spacing' alt='user avatar' />
+        <img className='navbar-text mr-3' alt='user avatar' />
         <NavItem>
             <NavLink tag={Link} to='/' onClick={event => props.dispatch(logoutUser())}>Logout</NavLink>
         </NavItem>

3fee294b041b8b518508a2751b9a5de154a2a23b
Schwan <50006@icf.com>
2018-07-27 19:21:37 -0500

    Added style to newQuestion

--- src/components/newQuestion.js
+++ src/components/newQuestion.js
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { Card, Button, CardHeader, CardBody,
     CardTitle, CardText, Form, FormGroup, Input } from 'reactstrap';
+import './newQuestion.css'
 import { saveQuestion } from '../actions/questions';
 
 class NewQuestion extends React.Component {
@@ -38,16 +39,19 @@ class NewQuestion extends React.Component {
     render() {
         return (
             <Card>
-                <CardHeader>Create New Question</CardHeader>
+                <CardHeader className='h4 text-center'>Create New Question</CardHeader>
                 <CardBody>
                     <Form> 
+                        <strong className='text-secondary'>Complete the question:</strong>
+                        <p className='h5 mt-3'>Would you rather ...</p>
                         <FormGroup>
                         <Input type="text" onChange={this.onOptionOneChange} value={this.state.optionOneText} placeholder="Enter Option One Text Here" />
                         </FormGroup>
+                        <p className='h6 text-center'>Or</p>
                         <FormGroup>
                         <Input type="text" onChange={this.onOptionTwoChange} value={this.state.optionTwoText} placeholder="Enter Option Two Text Here" />
                         </FormGroup>
-                        <Button onClick={this.saveNewQuestion}>Submit</Button>
+                        <Button className='btn-block' onClick={this.saveNewQuestion}>Submit</Button>
                     </Form>
                 </CardBody>
             </Card>

a5cff1788bc2cde694d272c11ccccf69f905900f
Christopher Schwan <ChrisSchwan8009@hotmail.com>
2018-07-29 17:37:53 -0500

    Added percentage graphic to result card

--- src/components/answerResultCard.js
+++ src/components/answerResultCard.js
@@ -1,21 +1,33 @@
 import React, { Component, Fragment } from 'react';
 import { Card, CardHeader, CardBody,
     CardText, CardTitle, Form, FormGroup, Label, Input, Row, Col, Button } from 'reactstrap';
+import 'react-circular-progressbar/dist/styles.css';
+import CircularProgressbar from 'react-circular-progressbar';
 
-export const AnswerResultCard = (props) => (
-    <Card>
+export const AnswerResultCard = (props) => {
+    let percentage = Math.round((props.numberVotes/props.totalVotes) * 100);
+    return (<Card>
         <CardBody>
-            {props.optionText}
-            <br />
-            {`${props.numberVotes} out of ${props.totalVotes} votes`}
-            {props.loginUserAnswer !== false && 
-            (
-                <Fragment>
+            <Row>
+                <Col>
+                    {props.optionText}
                     <br />
-                        Your Vote
-                </Fragment>
-            )
-            }
+                    {`${props.numberVotes} out of ${props.totalVotes} votes`}
+                    {props.loginUserAnswer !== false && 
+                        (
+                            <Fragment>
+                                <br />
+                                    Your Vote
+                            </Fragment>
+                        )
+                        }
+                </Col>
+                <Col>
+                    <div style={{ width: '100px' }}>
+                        <CircularProgressbar percentage={percentage} text={`${percentage}%`} />
+                    </div>
+                </Col>
+            </Row>
             </CardBody>
-    </Card>
-);
+    </Card>);
+}

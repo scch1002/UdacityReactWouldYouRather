1823d57a4f8ee0a1de81c3101633f60feba58203
Schwan <50006@icf.com>
2018-07-30 08:38:11 -0500

    Further styling of questionListItem

--- src/components/questionListItem.js
+++ src/components/questionListItem.js
@@ -17,13 +17,15 @@ export const QuestionListItem = connect(
                         <img width='150px' height='150px' src={author.avatarURL} />
                     </Col>
                     <Col>
-                        <CardTitle tag='strong'>Would you rather</CardTitle>
+                        <CardTitle className='h5'>Would You Rather ...</CardTitle>
                         <CardText>
-                            {props.question.optionOne.text}
-                                <br />
-                                or
-                                <br />
-                            {props.question.optionTwo.text}
+                            <p class='text-center'>
+                                {props.question.optionOne.text}
+                                    <br />
+                                    OR
+                                    <br />
+                                {props.question.optionTwo.text}
+                            </p>
                         </CardText>
                         <Button className='btn-block' tag={Link} to={`/questions/${props.question.id}`}>View Pull</Button>
                     </Col>
